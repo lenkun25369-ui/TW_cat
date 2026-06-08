96646655bb2b1816a66c32f8285cab8be721163d
lenkun25369-ui <lenkun25369@proton.me>
2026-06-08 14:06:31 +0800

    Update config.js

--- config.js
+++ config.js
@@ -56,7 +56,7 @@ window.SMART_CONFIGS = {
     name: "Muen SMART on FHIR Server",
     clientId: "muen",
     redirectUri: "https://lenkun25369-ui.github.io/TW_cat/index.html",
-    scope: "launch launch/patient openid fhirUser patient/Patient.read patient/Observation.read",
+    scope: "launch/patient openid fhirUser patient/Patient.read patient/Observation.read",
     defaultIss: "http://192.168.2.213:8080/fhir",
     defaultMode: "patient-observations",//defaultMode: "patient-observations",
     fixedResourceUrl: "",
